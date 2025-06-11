a7a073d474535445d735a045af4240a5d8a6f1e9
xxy <xingyue.xia@outlook.com>
2025-06-11 22:53:53 +0800

    Day07：添加购物车

--- VinoHouse-pojo/src/main/java/com/VinoHouse/dto/ShoppingCartDTO.java
+++ VinoHouse-pojo/src/main/java/com/VinoHouse/dto/ShoppingCartDTO.java
@@ -6,8 +6,8 @@ import java.io.Serializable;
 @Data
 public class ShoppingCartDTO implements Serializable {
 
-    private Long beverageId;
+    private Long dishId;  // 前端硬编码，不能修改为 beverageId
     private Long setmealId;
-    private String beverageFlavor;
+    private String dishFlavor;  // 前端硬编码，不能修改为 beverageFlavor
 
 }
