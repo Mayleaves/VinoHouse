34efb0e595ed9d8132eb38df904cc697e7f2aad0
xxy <xingyue.xia@outlook.com>
2025-06-11 23:06:14 +0800

    Day07：删除购物车中一个商品

--- VinoHouse-server/src/main/java/com/VinoHouse/controller/user/ShoppingCartController.java
+++ VinoHouse-server/src/main/java/com/VinoHouse/controller/user/ShoppingCartController.java
@@ -52,14 +52,14 @@ public class ShoppingCartController {
         return Result.success();
     }
 
-//    /**
-//     * 删除购物车中一个商品
-//     */
-//    @PostMapping("/sub")
-//    @ApiOperation("删除购物车中一个商品")
-//    public Result sub(@RequestBody ShoppingCartDTO shoppingCartDTO) {
-//        log.info("删除购物车中一个商品，商品：{}", shoppingCartDTO);
-//        shoppingCartService.subShoppingCart(shoppingCartDTO);
-//        return Result.success();
-//    }
+    /**
+     * 删除购物车中一个商品
+     */
+    @PostMapping("/sub")
+    @ApiOperation("删除购物车中一个商品")
+    public Result sub(@RequestBody ShoppingCartDTO shoppingCartDTO) {
+        log.info("删除购物车中一个商品，商品：{}", shoppingCartDTO);
+        shoppingCartService.subShoppingCart(shoppingCartDTO);
+        return Result.success();
+    }
 }
