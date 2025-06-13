7ba766c76579f2419584d20b4d550b8d52444961
xxy <xingyue.xia@outlook.com>
2025-06-13 17:12:53 +0800

    Day09：取消订单

--- VinoHouse-server/src/main/java/com/VinoHouse/controller/user/OrderController.java
+++ VinoHouse-server/src/main/java/com/VinoHouse/controller/user/OrderController.java
@@ -74,16 +74,16 @@ public class OrderController {
         return Result.success(orderVO);
     }
 
-//    /**
-//     * 用户取消订单
-//     */
-//    @PutMapping("/cancel/{id}")
-//    @ApiOperation("取消订单")
-//    public Result cancel(@PathVariable("id") Long id) throws Exception {
-//        orderService.userCancelById(id);
-//        return Result.success();
-//    }
-//
+    /**
+     * 用户取消订单
+     */
+    @PutMapping("/cancel/{id}")
+    @ApiOperation("取消订单")
+    public Result cancel(@PathVariable("id") Long id) throws Exception {
+        orderService.userCancelById(id);
+        return Result.success();
+    }
+
 //    /**
 //     * 再来一单
 //     */
