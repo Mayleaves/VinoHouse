79dc2e088a7ea50b163f61c10f124fe0baccc496
xxy <xingyue.xia@outlook.com>
2025-06-15 15:50:40 +0800

    Day09：派送订单

--- VinoHouse-server/src/main/java/com/VinoHouse/controller/admin/OrderController.java
+++ VinoHouse-server/src/main/java/com/VinoHouse/controller/admin/OrderController.java
@@ -87,16 +87,16 @@ public class OrderController {
         return Result.success();
     }
 
-//    /**
-//     * 派送订单
-//     */
-//    @PutMapping("/delivery/{id}")
-//    @ApiOperation("派送订单")
-//    public Result delivery(@PathVariable("id") Long id) {
-//        orderService.delivery(id);
-//        return Result.success();
-//    }
-//
+    /**
+     * 派送订单
+     */
+    @PutMapping("/delivery/{id}")
+    @ApiOperation("派送订单")
+    public Result delivery(@PathVariable("id") Long id) {
+        orderService.delivery(id);
+        return Result.success();
+    }
+
 //    /**
 //     * 完成订单
 //     */
